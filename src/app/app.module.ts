53dc0c77c98b2803388a1454b2a4fcb14244230c
Thiagonovaesbatista <thiago.novaes.batista@gmail.com>
2020-05-08 19:54:25 -0300

    commit aula5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { CursosModule } from './cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
